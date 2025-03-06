c31946a108f68508baa919db2fefe4aa41d3399d
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-03-07 03:23:34 +0530

    Add files via upload

---
+++ Queens.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<char>> grid;
+
+bool canPlaceQueen(int row, int col, int n){
+    for(int i=row-1; i>=0; i--){  //check column
+        if(grid[i][col] == 'Q') return false;
+    }
+
+    for(int i=row-1; i>=0; i--){
+        if(grid[i][col] == 'Q') return false;
+    }
+
+    for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){    // check left diagonal
+        if(grid[i][j] == 'Q') return false;    // we are attacked
+    }
+    return true;    // No attack
+}
+
+void f(int row, int n){
+    if(row==n){
+        for(int i=0; i<n; i++){    // We got one possible answer
+            for(int j=0; j<n; j++){
+                cout<<grid[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return;
+    }
+    for(int col=0; col<n; col++){
+        if(canPlaceQueen(row, col, n)){
+            grid[row][col] = 'Q';
+            f(row+1, n);
+            grid[row][col] = '.';
+        }
+    }
+}
+
+vector<vector<string>>nQueen(int n){
+    grid.resize(n, vector<char>(n, '.' ));
+    f(0, n);
+}
+
+int main(){
+    nQueen(4);
+    return 0;
+}
